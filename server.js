da584fff8b6d3cac25573f64bdeebf9351f68d41
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2022-05-22 13:22:26 +0800

    added some mongodb stuff

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const app = express();
 const port = process.env.PORT || 80;
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 app.use(cors());
+
+mongoose.connect(process.env.MONGO_CONNECT, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
